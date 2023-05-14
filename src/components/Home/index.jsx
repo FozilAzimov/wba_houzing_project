eddf24f056b5f1df9d7006645b938bf92f9b8c85
FozilAzimov <foziljon.azimov@yandex.ru>
2023-05-14 22:05:51 +0500

    houzing projet

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Input, Button } from '../Generic';
+import { Input } from '../Generic';
 import { Container } from './style';
 
 export default function Home () {
